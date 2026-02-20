a06e080bb53074fb96d3e35bd8f53981d66cf29e
Santiago-Beltran1 <pedrazasantiago837@gmail.com>
2026-02-20 17:01:27 -0500

    Trabajo terminado de la clase del 20 de febrero en Angular, se crea la interfaz de tetxo usuario-asistente

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MensajeChat } from '../../../models/chat';
+import { FormsModule } from '@angular/forms';  
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
@@ -14,9 +15,11 @@ export class Chat {
     console.log('Error al cargar la imagen del usuario');
   }
   mensajes: MensajeChat[] = []
-  cargandoHistorial = true
+  cargandoHistorial = false
   asistenteEscribiendo = true
   asistenteEnviando = false
+  mensajeTexto=""
+  enviandoMensaje=""
   cerrarSesion(){}
 
   trackByMensaje(index: number, mensaje: MensajeChat){}
@@ -43,6 +46,35 @@ export class Chat {
     },{
       id:'id2',
       contenido:'Hola soy tu asistente',
+      tipo: 'Asistente',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'a1'
+    },
+    {
+      id:'id1',
+      contenido:'Me puedes ayudar con mi problema?',
+      tipo: 'Usuario',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'u1'
+    },{
+      id:'id2',
+      contenido:'Claro dime lo qué te sucede',
+      tipo: 'Asistente',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'a1'
+    },{
+      id:'id1',
+      contenido:'Mandame emojis de pizza',
+      tipo: 'Usuario',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'u1'
+    },{
+      id:'id2',
+      contenido:'🍕🍕🍕🍕',
       tipo: 'Asistente',
       fechaEnvio: new Date(ahora.getTime()),
       estado: 'Enviado',
--- src/models/chat.ts
+++ src/models/chat.ts
@@ -1,17 +1,17 @@
 export interface MensajeChat{
-    id: String,
-    contenido: String,
-    usuarioId: String,
+    id: string,
+    contenido:  string,
+    usuarioId: string,
     fechaEnvio: Date,
     estado: 'Enviado' | 'Enviando' | 'Error' | 'Temporal'
     tipo: 'Usuario' | 'Asistente'
 }
 
 export interface ConversacionChat{
-    id: String,
-    usuarioId: String,
+    id: string,
+    usuarioId: string,
     mensajes: MensajeChat,
     ultimaActividad: Date,
     fechaCreacion: Date,
-    titulo: String,
+    titulo: string,
 }
