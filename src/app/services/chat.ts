d4269218be07440bcfbfa6e1cc7c9b693c759e78
Santiago-Beltran1 <pedrazasantiago837@gmail.com>
2026-03-01 21:19:09 -0500

    Inicio de Sesión con google y primera parte del servicio firebase y gemini

--- src/app/services/chat.ts
+++ src/app/services/chat.ts
@@ -1,8 +1,57 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { MensajeChat } from '../../models/chat';
+import { AuthService } from './auth';
+import { FirebaseService } from './firebase';
+import { BehaviorSubject } from 'rxjs';
+
+// Vamos a generar un mock del servicio gemini
+const GeminiServiceMock = {
+  convertirHistorialGemini: (Historial: MensajeChat[])=> Historial,
+  EnviarMensaje: async(contenido: string, Historial: any)=> 'Respuesta desde el servicio de gemini de tipo Mock, esta respuesta siempre va a ser la misma'
+}
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
-export class ChatService {
 
+export class ChatService {
+  private authService = inject(AuthService)
+
+  private FirebaseService = inject(FirebaseService)
+
+  private mensajeSubject = new BehaviorSubject<MensajeChat[]>([]);
+
+  public mensaje$ = this.mensajeSubject.asObservable();
+
+  private CargandoHistorial = false;
+
+  private AsistenteRespondiendo = new BehaviorSubject<Boolean>(false);
+
+  private AsistenteRespondiendo$ = this.AsistenteRespondiendo.asObservable();
+
+  async InicializarChat(usuarioId: string): Promise<void>{
+    if(!this.CargandoHistorial){
+      return;
+    }
+
+    this.CargandoHistorial = true;
+    try {
+      this.FirebaseService.obtenerMensajesUsuario(usuarioId).subscribe({
+        next: (mensajes)=>{
+          // Actualizando BehaviorSubject
+          this.mensajeSubject.next(mensajes)
+          this.CargandoHistorial = false;
+        },
+        error: (error) =>{
+          console.log("error al cargar el historial", error)
+          this.CargandoHistorial = false
+          // Cargar el BehaviorSubject
+        }
+      });
+    }catch(error){
+      console.log("Error inesperado:", error);
+      this.CargandoHistorial = false;
+    }
+
+  }
 }
--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -1,8 +1,15 @@
-import { Injectable, inject } from '@angular/core';
-import { Firestore, Timestamp, collection } from '@angular/fire/firestore';
-import { MensajeChat } from '../../models/chat';
+import { Injectable, Query, inject } from '@angular/core';
+import { Firestore, 
+  Timestamp, 
+  collection, 
+  query, 
+  where,
+  onSnapshot, 
+  DocumentData, 
+  QuerySnapshot} from '@angular/fire/firestore';
+import { ConversacionChat, MensajeChat } from '../../models/chat';
 import { addDoc } from 'firebase/firestore';
-import { Observable } from 'rxjs';
+import { Observable, timestamp } from 'rxjs';
 
 @Injectable({
   providedIn: "root"
@@ -46,8 +53,68 @@ export class FirebaseService {
     }
   }
 
-  obtenerMensajesUsuario(userId: int): Observable${
-    //Filtrar que los mensajes que se muestran sean los mensajes del usuario autenticado
+  //Filtrar que los mensajes que se muestran sean los mensajes del usuario autenticado
+  obtenerMensajesUsuario(usuarioId: string): Observable<MensajeChat[]>{
+    return new Observable ( observer =>{
+      const consulta = query(
+        collection(this.firestore, 'mensajes'),
+        where('usuarioId', "==", usuarioId)
+      )
+      // Configurar el listener para que funcione en tiempo real snapshot
+      const unSuscribe = onSnapshot(
+        consulta, 
+        (snapshot: QuerySnapshot<DocumentData>)=>{
+          const mensajes : MensajeChat[] = snapshot.docs.map( doc =>{
+            const data = doc.data();
+            return{
+              id: doc.id,
+              usuarioId: data['usuarioId'],
+              contenido: data['contenido'],
+              estado: data['estado'],
+              tipo: data['tipo'],
+              // Recordemos que firebase guarda TIMESTAMP y angular trabaja con DATE
+              fechaEnvio: data['fechaEnvio'].toDate()
+            } as MensajeChat;
+          });
+
+          
+          // ordenar los mensajes desde el más reciente al más antiguo
+          mensajes.sort((a,b)=>a.fechaEnvio.getTime() - b.fechaEnvio.getTime())
+          observer.next(mensajes);
+        },
+        error => {
+          console.error('error al escuchar los mensajes')
+          observer.error(error);
+        }
+      );
+      // Se retorna una suscripción al usuario
+      return ()=>{
+        unSuscribe
+      }
+    });
+    // Gestionar el obtener id de usuario por medio de un mensaje
   }
+
+  async guardarConversacion(conversacion: ConversacionChat): Promise<void>{
+    try{
+      const coleccionConversaciones = collection(this.firestore, 'conversaciones');
+      // Las conversaciones para enviarlas a firestore 
+      const conversacionGuardar = {
+        ...conversacion,
+        fechaCreacion : Timestamp.fromDate(conversacion.fechaCreacion),
+        ultimaActividad : Timestamp.fromDate(conversacion.ultimaActividad),
+        // Conversación de la fechaEnvio del MensajeChat
+        mensajes : conversacion.mensajes.map(mensaje => ({
+          ...mensaje, 
+          fechaEnvio: Timestamp.fromDate(mensaje.fechaEnvio)
+        }))
+      };
+      await addDoc(coleccionConversaciones, conversacionGuardar);
+    }catch(error){
+      console.error('Error al guardar la conversación', error)
+      throw error;
+    }
+  }
+
 }
 
